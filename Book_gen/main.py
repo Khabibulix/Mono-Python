4d2324f6ea4419c95f0795549ee6cff337c4e2fa
Khabibulix <80721211+Khabibulix@users.noreply.github.com>
2023-05-30 21:17:40 +0200

    Delete Book_gen directory

--- Book_gen/main.py
+++
@@ -1,8 +0,0 @@
-import random
-from data import *
-from Application import Application
-from Book_Chooser import Book_Chooser
-
-if __name__ == "__main__":
-	window = Application()
-	window.mainloop()
