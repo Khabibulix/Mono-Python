014b45e5c4764a04d0fbc190b0fcf0f211e00fae
Khabibulix <unitetempete@gmail.com>
2023-06-01 09:45:13 +0200

    Moving on to dictionnary and to adding items

--- 2023/BookWorm/src/main.py
+++ 2023/BookWorm/src/main.py
@@ -1,6 +1,6 @@
-list_of_books = []
+list_of_books = {}
 
-#Asking and managing input
+# Asking and managing input
 possible_choices = ["1", "2", "3"]
 user_choice = ""
 while user_choice != "exit":
@@ -8,6 +8,22 @@ while user_choice != "exit":
                         "Do you want to add a book? Press 2 \n"
                         "Do you want to delete a book from the list? Press 3.\n")
     if user_choice in possible_choices:
-        print(f"You chose {user_choice}!")
+
+        # We check if the list is empty else we print the list
+        if user_choice == "1" and len(list_of_books) == 0:
+            print("The list is empty yet.")
+        if user_choice == "1" and len(list_of_books) > 0:
+            # TODO: Make a nicer print!
+            print(list_of_books)
+
+        if user_choice == "2":
+            author = input("Please enter the author: ")
+            title = input("Please enter the title: ")
+            list_of_books.update({"Book": {"author": author, "title": title}})
+
+
+
+
+
     else:
         print("Not a valid choice!")
