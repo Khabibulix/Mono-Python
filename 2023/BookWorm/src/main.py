b22d89d3310bf10b8a43e1ed552871f98ee29842
Khabibulix <unitetempete@gmail.com>
2023-06-04 17:01:53 +0200

    Searching is complete, it's good enough for this stade

--- 2023/BookWorm/src/main.py
+++ 2023/BookWorm/src/main.py
@@ -1,15 +1,8 @@
-"""
-Différents inputs à checker: Titre et auteur, on veut un titre de moins de 200 caractères, un auteur de moins de 40 caractères
 
-"""
-
-
-# TODO Ajouter option de recherche
-
-list_of_books = [["Goggins", "Can't hurt me"], ["Zola", "Germinal"]]
-
-# Functions
-
+list_of_books = [["Goggins", "Can't hurt me"],
+                 ["Zola", "Germinal"],
+                 ["Baudelaire", "Les fleurs du mal"],
+                 ["Zola", "La Bête Humaine"]]
 
 def pretty_print(list_to_print):
     """["author", "title"] -> "AUTHOR, title\""""
@@ -28,10 +21,8 @@ def adding_book_to_list():
     if isinstance(title, str) and len(title) > 3 and isinstance(author, str) and len(author) > 10:
         list_of_books.append([author, title])
     else:
-        print("Please enter a correct book/author")
+        print("Please enter a correct book/author\n")
         adding_book_to_list()
-
-    return list_of_books
 
 
 def delete_book():
@@ -40,14 +31,28 @@ def delete_book():
     list_of_books.pop(int(book_deleted) - 1)
 
 
+def search_for_book(string_to_search):
+    possible_matches = []
+    for item in list_of_books:
+        if string_to_search.lower() in item[0].lower() or string_to_search.lower() in item[1].lower():
+            possible_matches.append(item)
+    pretty_print(possible_matches)
+
+
+
+
+
+
 # Asking and managing input
-possible_choices = ["1", "2", "3", "exit"]
+possible_choices = ["1", "2", "3","4","exit"]
 user_choice = ""
 while user_choice != "exit":
 
     user_choice = input("\nDo you want to look at a book? Press 1. \n"
                         "Do you want to add a book? Press 2 \n"
                         "Do you want to delete a book from the list? Press 3.\n"
+                        "Do you want to search for a book? Press 4.\n"
+                        "If you want to quit, write exit.\n"
                         )
     if user_choice in possible_choices:
 
@@ -62,6 +67,13 @@ while user_choice != "exit":
 
         if user_choice == "3":
             delete_book()
+
+        if user_choice == "4":
+            searching_input = input("Please enter the name of the author, or the book:\n")
+            search_for_book(searching_input)
+            user_consent = input("Shall we take you back? Y/N:\n")
+            if user_consent == "Y":
+                continue
 
     else:
         print("Not a valid choice!")
