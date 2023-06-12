c762c13c5ffe85f2f4256f311204058e43baa080
Khabibulix <unitetempete@gmail.com>
2023-06-12 20:36:28 +0200

    Working on testing my Library class

--- 2023/BookWorm/tests/tests_Library.py
+++ 2023/BookWorm/tests/tests_Library.py
@@ -1,12 +1,25 @@
 from ..src.Library import *
 
 import pytest
+import os
+import mock
+import builtins
 
+CURRENT_PATH = os.path.dirname(__file__)
+path_to_output_file = os.path.relpath('..\\src\\output.txt', CURRENT_PATH)
+library = Library()
 
 def test_possible_choices():
-    assert len(POSSIBLE_CHOICES) == 6
+    assert len(POSSIBLE_CHOICES) == 6 and "exit" in POSSIBLE_CHOICES
 
-
+def test_adding_book():
+    with mock.patch.object(builtins, 'input', lambda _: 'Emile Zolu'):
+        library.adding_book()
+        try:
+            output_file = open(path_to_output_file, "r")
+            print(output_file.read())
+        finally:
+            output_file.close()
 
 
 
