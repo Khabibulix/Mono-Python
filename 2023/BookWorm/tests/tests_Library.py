393b44288a09575f8cfc80dc29ebea47f2123a15
Khabibulix <unitetempete@gmail.com>
2023-06-12 20:48:23 +0200

    My test of adding_book is raising a strange behaviour, adding a book doesnt add to the output file immediately

--- 2023/BookWorm/tests/tests_Library.py
+++ 2023/BookWorm/tests/tests_Library.py
@@ -13,13 +13,10 @@ def test_possible_choices():
     assert len(POSSIBLE_CHOICES) == 6 and "exit" in POSSIBLE_CHOICES
 
 def test_adding_book():
-    with mock.patch.object(builtins, 'input', lambda _: 'Emile Zolu'):
+    current_size_of_file = os.path.getsize(path_to_output_file)
+    with mock.patch.object(builtins, 'input', lambda _: 'Emile Zola'):
         library.adding_book()
-        try:
-            output_file = open(path_to_output_file, "r")
-            print(output_file.read())
-        finally:
-            output_file.close()
+        assert os.path.getsize(path_to_output_file) > current_size_of_file
 
 
 
