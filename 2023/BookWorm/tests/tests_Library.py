28ff9f53499fd4c922444e3730184ebd4a781f95
Khabibulix <unitetempete@gmail.com>
2023-06-14 11:37:21 +0200

    Test to check location of output file complete and working

--- 2023/BookWorm/tests/tests_Library.py
+++ 2023/BookWorm/tests/tests_Library.py
@@ -1,6 +1,6 @@
 from ..src.Library import *
-from pathlib import Path
 
+from pathlib import Path, WindowsPath
 import pytest
 import os
 import mock
@@ -8,19 +8,22 @@ import builtins
 
 
 PATH_TO_BOOKWORM_DIRECTORY = Path(__file__).parents[1]
-OUTPUT_FILE_PATH = Path.joinpath(PATH_TO_BOOKWORM_DIRECTORY, "src\output.txt")
+OUTPUT_FILE_DIRECTORY = Path.joinpath(PATH_TO_BOOKWORM_DIRECTORY, "src")
 
-# TODO Grab size of file using Pathlib
-# current_size_of_file = os.path.getsize(path_to_output_file)
+current_size_of_file = os.path.getsize(OUTPUT_FILE_DIRECTORY)
+
 library = Library()
 
 def test_possible_choices():
     assert len(POSSIBLE_CHOICES) == 6 and "exit" in POSSIBLE_CHOICES
 
-def test_adding_book():
-    with mock.patch.object(builtins, 'input', lambda _: 'Emile Zola'):
-        library.adding_book()
-        assert os.path.getsize(path_to_output_file) > current_size_of_file
+def test_path_to_output_file_is_correct():
+    assert OUTPUT_FILE_DIRECTORY == WindowsPath("C:/Users/33623/Documents/Mono-Python/2023/BookWorm/src")
+
+#CODE FOR TESTING ADD FUNCTION TO FIX
+# with mock.patch.object(builtins, 'input', lambda _: 'Emile Zola, Germinal'):
+#     library.adding_book(OUTPUT_FILE_DIRECTORY)
+#     assert os.path.getsize(OUTPUT_FILE_DIRECTORY) > current_size_of_file
 
 
 
