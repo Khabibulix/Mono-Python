93157d03545017f506da528d635e4a011c73108d
Khabibulix <unitetempete@gmail.com>
2023-06-16 11:25:26 +0200

    First container ok

--- 2023/BookWorm/src/GUI.py
+++ 2023/BookWorm/src/GUI.py
@@ -1,18 +1,23 @@
 from tkinter import Tk, Button, Label, Frame
 
+TOTAL_WIDTH_OF_WINDOW = 800
+TOTAL_HEIGHT_OF_WINDOW = 400
+TOTAL_HEIGHT_OF_TOP_MENU = 15
+
 class GUI(Tk):
 
     def __init__(self):
         super().__init__()
 
         #Frames
-        left_container = Frame(self, width=50, height=50, bg="blue")
-        left_container.place(x=15, y=30)
-        
+        left_container = Frame(self, width=320, height=350, relief="raised", bg="lightgray")
+        left_container.place(x=TOTAL_HEIGHT_OF_TOP_MENU, y=30)
+
         #Window config
-        self.geometry("600x400")
+        self.geometry(f"{TOTAL_WIDTH_OF_WINDOW}x{TOTAL_HEIGHT_OF_WINDOW}")
         self.title("BookWorm")
 
 
 window = GUI()
+window.resizable(False, False)
 window.mainloop()
