62804249182ff75b5124fd1a4bc744a2ff9784bb
Khabibulix <unitetempete@gmail.com>
2023-06-16 11:35:58 +0200

    Placing button menu

--- 2023/BookWorm/src/GUI.py
+++ 2023/BookWorm/src/GUI.py
@@ -2,7 +2,9 @@ from tkinter import Tk, Button, Label, Frame
 
 TOTAL_WIDTH_OF_WINDOW = 800
 TOTAL_HEIGHT_OF_WINDOW = 400
-TOTAL_HEIGHT_OF_TOP_MENU = 15
+MARGIN_LEFT = 15
+# WindowW - LeftContainerW - InitialPlacement * 4
+X_POS_FOR_RIGHT_CONTAINER = TOTAL_WIDTH_OF_WINDOW - 320 - 120
 
 class GUI(Tk):
 
@@ -10,8 +12,11 @@ class GUI(Tk):
         super().__init__()
 
         #Frames
-        left_container = Frame(self, width=320, height=350, relief="raised", bg="lightgray")
-        left_container.place(x=TOTAL_HEIGHT_OF_TOP_MENU, y=30)
+        left_container = Frame(self, width=320, height=350, borderwidth=2, relief="groove", bg="lightgray")
+        left_container.place(x=MARGIN_LEFT, y=30)
+
+        right_button_container = Frame(self, width=420, height=70, borderwidth=2, relief="groove", bg="gray")
+        right_button_container.place(x=X_POS_FOR_RIGHT_CONTAINER, y=30)
 
         #Window config
         self.geometry(f"{TOTAL_WIDTH_OF_WINDOW}x{TOTAL_HEIGHT_OF_WINDOW}")
