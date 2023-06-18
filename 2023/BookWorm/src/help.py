23febb637337574ecca58b1cca7caf7223f52154
Khabibulix <unitetempete@gmail.com>
2023-06-18 15:17:51 +0200

    Adding more messages for user

--- 2023/BookWorm/src/help.py
+++ 2023/BookWorm/src/help.py
@@ -1,9 +1,15 @@
-ADDING_HELP_MESSAGE = ""
+ADDING_HELP_MESSAGE = "To add a book, we'll ask one simple task." \
+                      " Please enter the name of the book you want to add in this format: \n" \
+                      "NAME OF AUTHOR, title of book"
 
 DELETING_HELP_MESSAGE = ""
 
 UPDATING_HELP_MESSAGE = ""
 
-MAIN_HELP_MESSAGE = ""
+MAIN_HELP_MESSAGE = "Welcome to BookWorm. \n" \
+                    " This application is designed to manage your library with ease. " \
+                    "\n"
 
 SEARCH_HELP_MESSAGE = ""
+
+EMPTY_INPUT_BOX_MESSAGE = "Did you forgot to enter any text in the text box?"
