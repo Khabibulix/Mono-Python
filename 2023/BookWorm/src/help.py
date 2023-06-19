84e9ea1d9368678db6cd2391ab4caba311a90890
Khabibulix <unitetempete@gmail.com>
2023-06-19 11:04:38 +0200

    Adding new message for missing coma which we split on

--- 2023/BookWorm/src/help.py
+++ 2023/BookWorm/src/help.py
@@ -1,6 +1,5 @@
-ADDING_HELP_MESSAGE = "To add a book, we'll ask one simple task." \
-                      " Please enter the name of the book you want to add in this format: \n" \
-                      "NAME OF AUTHOR, title of book"
+ADDING_HELP_MESSAGE = "Enter the name of the book you want to add in this format: \n" \
+                      "Author, title of book"
 
 DELETING_HELP_MESSAGE = ""
 
@@ -12,4 +11,8 @@ MAIN_HELP_MESSAGE = "Welcome to BookWorm. \n" \
 
 SEARCH_HELP_MESSAGE = ""
 
-EMPTY_INPUT_BOX_MESSAGE = "Did you forgot to enter any text in the text box?"
+EMPTY_INPUT_BOX_MESSAGE = "Did you forgot to enter text in the text box?"
+
+INCORRECT_INPUT_MESSAGE = ""
+
+MISSING_COMA_INPUT_MESSAGE = "Did you forgot to enter your book in this format?: Author, Title"
