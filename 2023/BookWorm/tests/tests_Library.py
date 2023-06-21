2abce50d3426fdc9b60f6ac92d688502f1b711ea
Khabibulix <unitetempete@gmail.com>
2023-06-21 10:25:55 +0200

    Adding tests to check for adding_method, tests working

--- 2023/BookWorm/tests/tests_Library.py
+++ 2023/BookWorm/tests/tests_Library.py
@@ -14,14 +14,22 @@ current_size_of_file = os.path.getsize(OUTPUT_FILE_DIRECTORY)
 
 library = Library()
 
-def test_possible_choices():
-    assert len(POSSIBLE_CHOICES) == 6 and "exit" in POSSIBLE_CHOICES
-
 def test_path_to_output_file_is_correct():
     assert OUTPUT_FILE_DIRECTORY == WindowsPath("C:/Users/33623/Documents/Mono-Python/2023/BookWorm/src")
 
-#CODE FOR TESTING ADD FUNCTION TO FIX
-# with mock.patch.object(builtins, 'input', lambda _: 'Emile Zola, Germinal'):
+def test_adding_book_without_coma():
+    assert library.adding_book("Emile Zola Germinal") == "Missing coma"
+
+def test_adding_book_with_shitty_input():
+    assert library.adding_book("To, To") == "Nope"
+
+def test_adding_book_with_correct_input_one():
+    assert library.adding_book("Emile Zola, Germinal") == "Germinal written by Emile Zola has been successfully added to library!"
+
+def test_adding_book_with_correct_input_two():
+    assert library.adding_book("Rastapopoulos, Memories") == "Memories written by Rastapopoulos has been successfully added to library!"
+
+
 #     library.adding_book(OUTPUT_FILE_DIRECTORY)
 #     assert os.path.getsize(OUTPUT_FILE_DIRECTORY) > current_size_of_file
 
