d6b8ca8a9350a6d49ee035750799bdcf2047bb80
Khab <unitetempete@gmail.com>
2024-03-07 03:58:56 +0100

    Non functional yet, need to add a function in main.py to check if './output_files/expenses.txt' exists

--- Budget.py
+++ Budget.py
@@ -10,22 +10,23 @@ class Budget:
     def add_expense(self, expense_description, expense):
         """
         Add expense to expenses list
-        @param: (float) expense
         @param: (string) expense_description
+        @param: (float) expense
         """
-        with open("expenses.txt", "a") as file:
+        with open("./output_files/expenses.txt", "a") as file:
             file.write(expense_description + ":" + str(expense) + "\n")
     
+    #TODO Ugly representation
     def get_expenses(self):
         """
         Retrieve data from file
         @return (list) expenses
         """
-        with open("expenses.txt", "r") as file:
+        with open("./output_files/expenses.txt", "r") as file:
             if file == "":
                 print("You have currently no expenses!")
             else:
-                return file.readlines()
+                return [item.strip() for item in file.readlines()]
 
     def get_budget_remaining(self):
         """
@@ -36,17 +37,14 @@ class Budget:
         for expense in self.get_expenses():
             #cf forum.checkmk.com/t/internal-error-invalid-literal-for-int-with-base-10-when-opening-main-dashboard/31567/18
             budget_total += int(float(expense.split(":")[1]))
-        return budget_total
+        return self.initial_budget - budget_total
     
     def clear_budget(self):
         """ 
         Erases the content of the file, will be useful for creating new budget
         """
-        open("expenses.txt","w").close()
+        open("./output_files/expenses.txt","w").close()
 
 
-budget = Budget(1000)
-budget.add_expense(expense_description="Cheese", expense=51)
-budget.get_budget_remaining()
 
 
--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from Budget import Budget
-import sys
+import sys, os
 #____________________________________________________________
 # #TODO 
-# Mettre données dans un fichier texte ou CSV pour la sauvegarde
-# Stocker budget du mois séparément ou en dur, car 
-# trop chiant à chaque début de programme
-# Supprimer budget du mois dans le fichier programmatiquement
 # Faire le choix 3
 # Rajouter tests
+# Rajouter data visualisation avec un camembert par exemple
 #____________________________________________________________
 budget_for_month = Budget(1000)
 
@@ -27,7 +24,7 @@ def asking_for_expense_input():
     
     try:
         float(input_expense)
-        budget_for_month.add_expense(input_expense, input_description)
+        budget_for_month.add_expense(input_description, input_expense)
     except ValueError as te:
         print("Input for expense doesn't seem valid...")
             
@@ -38,14 +35,13 @@ def main():
     #input_budget = input(" How much do you have for this month?\n")
     while True:
 
-        input_instruction_choice = input("\nWhat do you want to do? Press 1 to add an expense, Press 2 to see all expenses for the month, Press 4 to quit:\n")
+        input_instruction_choice = input("\nWhat do you want to do?\n Press 1 to add an expense\n Press 2 to see all expenses for the month\n Press 3 to begin a new budget\n Press 4 to quit\n")
         
         if input_instruction_choice == "1":
             asking_for_expense_input()
-            #TODO Add another one?
            
         elif input_instruction_choice == "2":
-            budget_for_month.get_representation_for_all_expenses()
+            print(budget_for_month.get_expenses())
             print(f"You have {budget_for_month.get_budget_remaining()} euros left!")
         
         elif input_instruction_choice == "3":
@@ -55,7 +51,24 @@ def main():
             2) supprimer fichier texte pour repartir à zéro
             3) redéfinir un budget initial
             """
-            pass
+            while True:
+                new_file_name = input("Please enter the name of the new budget text file: ")
+                #TODO Checker si le file expenses.txt existe aussi!
+                if new_file_name[-4:] == ".txt":
+                    os.rename("./output_files/expenses.txt", "./output_files/" + new_file_name)
+                    open("./output_files/expenses.txt").close()
+                    break
+                else:
+                    print("The input must end by .txt")
+
+            while True:
+                new_initial_budget = input("Enter the new initial budget: ")
+                budget_of_the_month = Budget(new_initial_budget)
+                try:
+                    int(new_initial_budget)
+                    break
+                except TypeError as te:
+                    print("Invalid input")
         
         elif input_instruction_choice == "4":
             print(f"Hope you had a great time, bye!")
