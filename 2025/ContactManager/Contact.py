c9e6cca39037e937703ed279dd0fecf2a6ae75ed
Khabibulix <unitetempete@gmail.com>
2025-08-29 19:40:41 +0200

    CONTACT-- adding_method for ContactList class

--- 2025/ContactManager/Contact.py
+++ 2025/ContactManager/Contact.py
@@ -1,18 +1,26 @@
-class Contact:
+class ContactList:
     """
     Stocks contact in the form of a dictionary
+    @param: (dict) contacts
     """
-    def __init__(self):
+    def __init__(self, contacts):
+        self.contacts = contacts
+
+    def add_contact(self,contact_name, contact_number):
+        """
+        Add contact to ContactList object
+        @param: (string) contact_name
+        @param: (string) contact_number
+        """
+        self.contacts[contact_name] = contact_number
+
+    def search_contact(contact):
         pass
 
-    def add_contact(contact):
+    def remove_contact(contact):
         pass
 
-    def remove_contact():
+    def modify_contact(contact):
         pass
 
-    def modify_contact():
-        pass
 
-    def search_contact():
-        pass
--- 2025/ContactManager/main.py
+++ 2025/ContactManager/main.py
@@ -3,4 +3,8 @@ Features: Add contact, remove contact, modify contact, search contact
 Console app with different choices with inputs
 Output in CSV with name and phone number.
 """
+from Contact import ContactList
 
+contact_list = ContactList({})
+contact_list.add_contact("Phil", "0622435434")
+print(contact_list.contacts)
