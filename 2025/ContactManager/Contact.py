c5f97ba12c1632a65502ae2797a18a0569ceff65
Khabibulix <unitetempete@gmail.com>
2025-08-29 19:29:17 +0200

    CONTACT-- Preparing main structure of CLI app

---
+++ 2025/ContactManager/Contact.py
@@ -0,0 +1,18 @@
+class Contact:
+    """
+    Stocks contact in the form of a dictionary
+    """
+    def __init__(self):
+        pass
+
+    def add_contact(contact):
+        pass
+
+    def remove_contact():
+        pass
+
+    def modify_contact():
+        pass
+
+    def search_contact():
+        pass
---
+++ 2025/ContactManager/main.py
@@ -0,0 +1,6 @@
+"""
+Features: Add contact, remove contact, modify contact, search contact
+Console app with different choices with inputs
+Output in CSV with name and phone number.
+"""
+
