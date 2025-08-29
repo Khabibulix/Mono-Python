d4d8b17c73f897c9e18a043123d842c3abee7bcd
Khabibulix <unitetempete@gmail.com>
2025-08-29 20:20:19 +0200

    CONTACT-- Sphinx doc format, adding, modifying and deleting features added

--- 2025/ContactManager/Contact.py
+++ 2025/ContactManager/Contact.py
@@ -7,20 +7,44 @@ class ContactList:
         self.contacts = contacts
 
     def add_contact(self,contact_name, contact_number):
-        """
-        Add contact to ContactList object
-        @param: (string) contact_name
-        @param: (string) contact_number
+        """Add contact to contacts.
+
+        Args:
+            contact_name (string): Full name of contact
+            contact_number (string): Phone number of contact
         """
         self.contacts[contact_name] = contact_number
 
-    def search_contact(contact):
-        pass
+    def search_contact(self,contact_name):
+        """Search for contact knowing the name
 
-    def remove_contact(contact):
-        pass
+        Args:
+            contact_name (string): Full name of contact
 
-    def modify_contact(contact):
+        Returns:
+            int: Phone number of contact
+        """
+        return self.contacts.get(contact_name)
+
+    def remove_contact(self, contact_name):
+        """Remove contact knowing name
+
+        Args:
+            contact_name (string): Full name of contact to remove
+        """
+        self.contacts.pop(contact_name)
+
+    def modify_contact(self, contact_name, contact_number):
+        """Modify contact via his name
+
+        :param contact_name: Full name of contact to modify
+        :type contact_name: String
+        :param contact_number: Number of modified contact
+        :type contact_name: String
+        """
+        self.contacts[contact_name] = contact_number
+
+    def save_contacts(self):
         pass
 
 
--- 2025/ContactManager/main.py
+++ 2025/ContactManager/main.py
@@ -2,9 +2,29 @@
 Features: Add contact, remove contact, modify contact, search contact
 Console app with different choices with inputs
 Output in CSV with name and phone number.
+
+Helper function pour valider num de tel?
+Tests avec unittest
 """
 from Contact import ContactList
 
-contact_list = ContactList({})
-contact_list.add_contact("Phil", "0622435434")
-print(contact_list.contacts)
+def main():
+    contact_list = ContactList({})
+    contact_list.add_contact("Phil Lambert", "0622435434")
+    contact_list.add_contact("Arnaud Lefevre", "0690324356")
+    contact_list.add_contact("Damien Dujardin", "0664234398")
+    contact_list.add_contact("Arthur Delabranche", "0622435434")
+
+
+    print(contact_list.contacts)
+    print(contact_list.search_contact("Phil Lambert"))
+    contact_list.remove_contact("Phil Lambert")
+    print(contact_list.contacts)
+    contact_list.modify_contact("Damien Dujardin", "0722435364")
+    print(contact_list.contacts)
+
+
+
+
+if __name__ == "__main__":
+    main()
