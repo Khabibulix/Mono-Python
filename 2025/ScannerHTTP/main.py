a23497e9f4a5f8cf22db5f4edf5f11f74fd8282f
Khabibulix <unitetempete@gmail.com>
2025-09-18 22:01:11 +0200

    SCANNER__ Checking for availability for all links of page and separating internal and external links

--- 2025/ScannerHTTP/main.py
+++ 2025/ScannerHTTP/main.py
@@ -1,7 +1,7 @@
 # Display a webpage and its titles
 # Display all links in page and titles including the name of the link
 
-from utils import creating_soup, fetch_data_from_site, grab_all_links_from_existing_soup
+from utils import *
 
 url = "http://jenesuis.net/"
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -9,7 +9,13 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleW
 def main():
     site_content = fetch_data_from_site(url)
     soup = creating_soup(site_content)
-    print(grab_all_links_from_existing_soup(soup))
+    available_links = grab_all_links_from_existing_soup(soup, url)
+    internal_links, external_links = separate_internal_and_external_links(available_links, url)
+    print(internal_links)
+    print(external_links)
+    print(are_all_link_available(internal_links, url))
+    print(are_all_link_available(external_links, url))
+
 
 if __name__ == "__main__":
     main()
--- 2025/ScannerHTTP/utils.py
+++ 2025/ScannerHTTP/utils.py
@@ -14,15 +14,61 @@ def fetch_data_from_site(site_url):
 def creating_soup(html_content):
     return BeautifulSoup(html_content, 'lxml')
 
-def grab_all_links_from_existing_soup(soup):
+def grab_all_links_from_existing_soup(soup, base_url):
     """Extract only links from soup and returning them in an array
 
     :param soup: Already existing soup (HTML code parsed with bs4)
     :type soup: string
+    :param base_url: Base url, first crawled
+    :type base_url: string
     """
     link_array = []
     links = soup.find_all('a')
     for link in links:
-        link_array.append(link.get('href'))
+        if "http" in link:
+            link_array.append(link.get('href'))
+        else:
+            link_array.append(base_url + link.get('href'))
     return link_array
+
+def separate_internal_and_external_links(link_array, base_url):
+    """Returns two arrays, one of external links and one of internal links
+
+    :param link_array: String array of all availables links on the original page
+    :type link_array: array
+    :param base_url: Base url, first crawled
+    :type base_url: string
+    """
+    ext_links = []
+    int_links = []
+
+    for link in link_array:
+        if base_url in link:
+            int_links.append(link)
+        else:
+            ext_links.append(link)
+
+    return int_links, ext_links
+
+def are_all_link_available(link_array, base_url):
+    """Returning true if status code for all available links is OK
+
+    :param link_array: String array of all availables links on the original page
+    :type link_array: array
+    :param base_url: Base url, first crawled
+    :type base_url: string
+    """
+    status_code_array = []
+
+    if len(link_array) == 0:
+        return False
+
+    for link in link_array:
+        status_code = requests.get(link).status_code
+        status_code_array.append(status_code == 200)
+    
+    if all(status_code_array):
+        return True
+
+
     
