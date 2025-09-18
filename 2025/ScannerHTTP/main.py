1940fec5927355baa722a2c9ced6a0119d0ad8d3
Khabibulix <unitetempete@gmail.com>
2025-09-18 21:31:50 +0200

    SCANNER__ Extracting links from soup

--- 2025/ScannerHTTP/main.py
+++ 2025/ScannerHTTP/main.py
@@ -1,13 +1,15 @@
 # Display a webpage and its titles
 # Display all links in page and titles including the name of the link
 
-from utils import creating_soup, fetch_data_from_site
+from utils import creating_soup, fetch_data_from_site, grab_all_links_from_existing_soup
 
 url = "http://jenesuis.net/"
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 def main():
-    creating_soup(fetch_data_from_site(url))
+    site_content = fetch_data_from_site(url)
+    soup = creating_soup(site_content)
+    print(grab_all_links_from_existing_soup(soup))
 
 if __name__ == "__main__":
     main()
--- 2025/ScannerHTTP/utils.py
+++ 2025/ScannerHTTP/utils.py
@@ -12,5 +12,17 @@ def fetch_data_from_site(site_url):
     return r.text
 
 def creating_soup(html_content):
-    return BeautifulSoup(html_content, 'html_parser')
+    return BeautifulSoup(html_content, 'lxml')
+
+def grab_all_links_from_existing_soup(soup):
+    """Extract only links from soup and returning them in an array
+
+    :param soup: Already existing soup (HTML code parsed with bs4)
+    :type soup: string
+    """
+    link_array = []
+    links = soup.find_all('a')
+    for link in links:
+        link_array.append(link.get('href'))
+    return link_array
     
