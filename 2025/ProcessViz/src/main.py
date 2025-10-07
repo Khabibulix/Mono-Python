a88b150e8f4523984584a002bacd1746a576f883
Khabibulix <unitetempete@gmail.com>
2025-10-07 17:21:46 +0200

    PROCESS_VIZ__ Changing project layout

--- 2025/ProcessViz/src/main.py
+++ 2025/ProcessViz/src/main.py
@@ -4,6 +4,7 @@ def main():
     # get_processes()
     # print(datas)
     # print(get_infos_for_process_with_pid(104))
+    pass
 
 if __name__ == "__main__":
     main()
--- 2025/ProcessViz/test/test_process_manager.py
+++ 2025/ProcessViz/test/test_process_manager.py
@@ -1,16 +1,23 @@
-import unittest, random
-from process_manager import *
+import unittest, random, psutil
+from src.process_manager import *
 
 class TestProcessManager(unittest.TestCase):
 
     def test_that_list_of_processes_is_of_correct_type(self):
         self.assertIsInstance(get_processes(), dict)
     
-    def test_that_Python_is_running(self):
+    def test_that_System_is_running(self):
         current_processes = get_processes()
-        self.assertIsNotNone(current_processes["python.exe"])
+        self.assertIsNotNone(current_processes["System"])
     
     def test_that_key_is_present_in_datas(self):
         current_processes = get_processes()
-        keys = ["PID", "memory usage", "path", "time alive", "status", "connections"]
-        self.assertIsNotNone(current_processes["python.exe"].get(random.choice(keys)))
+        self.assertIsNotNone(current_processes["python.exe"].get("PID"))
+    
+    def test_get_infos_for_process_with_pid(self):
+        first_five_existing_pid = psutil.pids()[:5]
+        self.assertNotEqual(get_infos_for_process_with_pid(random.choice(first_five_existing_pid)), [])
+    
+    def test_get_processes_correctly_fill_datas(self):
+        get_processes()
+        self.assertGreater(len(datas), 0)
