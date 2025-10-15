bb4a79cee38f88a36b33b8fc26f523055f8fcc64
Khabibulix <unitetempete@gmail.com>
2025-10-15 17:55:53 +0200

    PROCESS_VIZ__ Getting all services OK with WMI

--- 2025/ProcessViz/utils.py
+++ 2025/ProcessViz/utils.py
@@ -1,4 +1,4 @@
-import hashlib, subprocess, os, psutil, win32service, win32serviceutil
+import hashlib, subprocess, os, psutil, wmi
 
 
 def grab_sha256_hash_of_process(path_of_process):
@@ -49,52 +49,24 @@ def is_deleted_executable(process: psutil.Process) -> bool:
     except (psutil.AccessDenied, psutil.NoSuchProcess):
         return False
 
-def open_session_SCM() -> dict:
-    scm_handle = win32service.OpenSCManager(
-        None,
-        None, 
-        win32service.SC_MANAGER_ENUMERATE_SERVICE
-    )
-    return scm_handle
-
-def close_session_SCM(scm) -> None:
-    win32service.CloseServiceHandle(scm)
-
 def get_services() -> dict:
-    service_map = {}
-    scm = open_session_SCM()
+    results = {}
 
-    services, _ = win32service.EnumServicesStatusEx(
-        scm,
-        win32service.SC_ENUM_PROCESS_INFO,  # Niveau d'info: on veut les PIDs
-        win32service.SERVICE_WIN32,         # Type: services classiques
-        win32service.SERVICE_ACTIVE,        # Seulement les services actifs
-        None                          # Resume handle pour pagination
-    )
+    for service in wmi.WMI().Win32_Service():
+        
+        try:
+            pid = int(service.ProcessID) if service.ProcessID not in (None, '') else 0
+        except Exception:
+            pid = 0
+        
+        name = service.Name
+        # display = getattr(service, 'DisplayName', None)
 
-    for svc in services:
-        pid = svc['ProcessId']
-        name = svc['ServiceName']
         if pid > 0:
-            service_map[pid] = name
-
-    # services = win32service.EnumServicesStatus(
-    #             scm,
-    #             win32service.SERVICE_WIN32,
-    #             win32service.SERVICE_STATE_ALL
-    #         )
-    
-    # for service in services:
-    #     name = service[0]
-    #     try:
-    #         status = win32serviceutil.QueryServiceStatus(name)
-    #         pid = status[-1]
-    #         if pid > 0:
-    #             service_map[pid] = name
-    #     except Exception:
-    #         pass
-    
-    close_session_SCM(scm)
-    return service_map
+            results.setdefault(pid, []).append(name)
+        
+    return results
 
 print(get_services())
+        
+
